0fa4ba128e8f74360f0eb224078772e260c7a9bb
YogiSoni-lazy <59290335+YogiSoni-lazy@users.noreply.github.com>
2023-10-18 10:55:03 +0530

    Update breakfix011-maintenanceLVM2.py

--- breakfix011-maintenanceLVM2.py
+++ breakfix011-maintenanceLVM2.py
@@ -4,15 +4,44 @@
 # All rights reserved.
 # No warranty, explicit or implied, provided.
 
-from labs.common import userinterface
+from labs import labconfig
 from labs.grading import Default
+from labs.common import steps, labtools, userinterface
 
+SKU = labconfig.get_course_sku().upper()
+
+_targets = ["servera"]
+_servera = "servera"
 
 class Breakfix011Maintenancelvm2(Default):
     __LAB__ = "breakfix011-maintenanceLVM2"
 
     def start(self):
-        items = []
+        items = [
+            {
+                "label": "Checking lab systems",
+                "task": labtools.check_host_reachable,
+                "hosts": _targets,
+                "fatal": True,
+            },
+            steps.run_command(
+                label="Configuring " + _servera,
+                hosts=[_servera],
+                command='''
+                pvcreate /dev/vdb;
+                vgcreate vg01 /dev/vdb;
+                lvcreate -L 800M -n lv01 vg01;
+                mkfs.xfs /dev/vg01/lv01;
+                mkdir /mnt/data;
+                mount /dev/vg01/lv01 /mnt/data;
+                echo '/dev/vg01/lv01 /mnt/data xfs defaults 0 0' | sudo  tee -a /etc/fstab;
+                yes | lvreduce -L 500M /dev/vg01/lv01
+                echo '/usr/bin/cp /etc/fstab /tmp/fstab-capture.out' >> /etc/rc.d/rc.local
+                echo '/usr/bin/cp /proc/self/mounts /tmp/mounts-capture.out' >> /etc/rc.d/rc.local
+                ''',
+                shell=True,
+            ),
+        ]
         userinterface.Console(items).run_items(action="Starting")
 
     def grade(self):
